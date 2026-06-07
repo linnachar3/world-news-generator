4bb6bb75c5918d9b5f179c2ee6ae1e2817f68ea6
linnachar3 <linnachar3@gmail.com>
2026-06-07 18:13:58 +0800

    manifest.js

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+    "display_name": "信息中心 · 小报童",
+    "js": "main.js",
+    "css": "style.css",
+    "author": "kitten",
+    "version": "1.0.0",
+    "description": "悬浮报纸图标，点击生成世界观新闻、本地动态与角色社媒"
+}
